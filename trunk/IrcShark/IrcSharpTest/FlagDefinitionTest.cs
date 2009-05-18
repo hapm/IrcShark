027c44b845042f4bbcdac754589391b06afcb788
alpha <alpha@d25722fd-da67-0410-b9ee-c2eca8124d78>
2009-05-18 18:42:54 +0000

    * IrcSharkTranslation/messages.po: changed author information
    * IrcSharpTest/FlagDefinitionTest.cs: added FlagDefinitionTest
    
    * IrcSharpTest/IrcSharpTest.csproj: added tests
    
    * IrcSharpTest/UserInfoTest.cs: added UserInfo test (implementations
      in UserInfo need to made following the definitions of this test)
    
    git-svn-id: http://svn.ircshark.net/ircshark@7 d25722fd-da67-0410-b9ee-c2eca8124d78

---
+++ trunk/IrcShark/IrcSharpTest/FlagDefinitionTest.cs
@@ -0,0 +1,39 @@
+// $Id$
+// 
+// Note:
+// 
+// Copyright (C) 2009 IrcShark Team
+//  
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using IrcSharp;
+using NUnit.Framework;
+
+namespace IrcSharpTest
+{
+	/// <summary>
+	/// test class for <see cref="IrcSharp.FlagDefinition"/>
+	/// </summary>
+	[TestFixture()]
+	public class FlagDefinitionTest
+	{		
+		[Test()]
+		public void Constructor()
+		{
+			FlagDefinition fd = new FlagDefinition('f', ModeArt.User);
+			Assert.IsNotNull(fd);
+		}
+	}
+}

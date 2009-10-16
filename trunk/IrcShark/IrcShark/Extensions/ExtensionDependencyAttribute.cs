6306213a1d1e81e624226abad556abc5a0f4df8e
alpha <alpha@d25722fd-da67-0410-b9ee-c2eca8124d78>
2009-10-16 15:45:28 +0000

    + ExtensionDependencyAttribute um Abhängigkeiten zwischen Extensions definieren zu können
    git-svn-id: http://svn.ircshark.net/ircshark@36 d25722fd-da67-0410-b9ee-c2eca8124d78

---
+++ trunk/IrcShark/IrcShark/Extensions/ExtensionDependencyAttribute.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: markus
+ * Datum: 16.10.2009
+ * Zeit: 17:06
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+
+namespace IrcShark.Extensions
+{
+	/// <summary>
+	/// Describes the level of the dependency
+	/// </summary>
+	public enum DependencyLevel {
+		/// <summary>
+		/// If the Extension needs the other Extension to run, 
+		/// it is a strict dependency 
+		/// </summary>
+		Strict,
+		
+		/// <summary>
+		/// If the dependency can be used but didn't need to be used
+		/// it is an optional dependency
+		/// </summary>
+		/// <remarks>
+		/// Use this only if you don't have direct references to the 
+		/// assembly of the other extension
+		/// </remarks>
+		Optional
+	}
+	
+	/// <summary>
+	/// The ExtensionDependencyAttribute allows to define dependencies of extensions.
+	/// </summary>
+	/// <remarks>
+	/// By applying this Attribute to an Extension class, you can define an dependency
+	/// to another Extension type. You only need the full type name of the other Extension
+	/// class. If the dependency should be strict (your Extension does not run without the 
+	/// other Extension, use the DependencyLevel.Strict else use DependencyLevel.Optional.
+	/// </remarks>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+	public class ExtensionDependencyAttribute : Attribute
+	{
+		/// <summary>
+		/// saves the type name
+		/// </summary>
+		private string typeName;
+		
+		/// <summary>
+		/// saves the level
+		/// </summary>
+		private DependencyLevel level;
+		
+		/// <summary>
+		/// Creates a new ExtensionDependencyAttribute instance with the given type name and level
+		/// </summary>
+		/// <remarks>
+		/// The typename isn't checked but will be compare to the real typenames of existing extensions.
+		/// </remarks>
+		/// <param name="fullTypeName">The full type name of the Extension, your Extension depends on</param>
+		/// <param name="level">The level of the dependency</param>
+		public ExtensionDependencyAttribute(string fullTypeName, DependencyLevel level)
+		{
+			typeName = fullTypeName;
+			this.level = level;
+		}
+		
+		/// <summary>
+		/// Gets the full type name of the Extension depended on by the Extension this Attribute was applied to.
+		/// </summary>
+		public string TypeName {
+			get { return typeName; }
+		}
+		
+		/// <summary>
+		/// Gets the dependency level of this dependency
+		/// </summary>
+		public DependencyLevel Level {
+			get { return level; }
+		}
+	}
+}

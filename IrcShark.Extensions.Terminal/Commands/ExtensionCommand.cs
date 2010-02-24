e288982f2a23c34c0626bcc0defd80fd2322d3d2
Alpha <alpha@ircshark.net>
2010-02-24 18:36:45 +0100

    fixed the license header in all files
    Signed-off-by: Alpha <alpha@ircshark.net>

--- IrcShark.Extensions.Terminal/Commands/ExtensionCommand.cs
+++ IrcShark.Extensions.Terminal/Commands/ExtensionCommand.cs
@@ -3,7 +3,7 @@
 // </copyright>
 // <author>$Author$</author>
 // <date>$LastChangedDate$</date>
-// <summary>Place a summary here.</summary>
+// <summary>Contains the ExtensionCommand class.</summary>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by

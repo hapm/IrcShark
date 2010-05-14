ef9fe3dcfebb8bef18b6597e2e7ad4eda6d142d7
Alpha <alpha@ircshark.net>
2010-05-14 22:22:27 +0200

    fixed bad merge
    Signed-off-by: Alpha <alpha@ircshark.net>

--- IrcShark.Extensions.Scripting.Msl/Parser.cs
+++ IrcShark.Extensions.Scripting.Msl/Parser.cs
@@ -1,34 +1,5 @@
 using System.Text;
 using System.Collections.Generic;
-// <copyright file="Parser.cs" company="IrcShark Team">
-// Copyright (C) 2009 IrcShark Team
-// </copyright>
-// <author>$Author$</author>
-// <date>$LastChangedDate$</date>
-// <summary>Contains the Parser class.</summary>
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-namespace IrcShark.Extensions.Scripting.Msl
-{
-    using System;
-    using System.CodeDom;
-    using System.CodeDom.Compiler;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Text;
-
 // <copyright file="MslScriptEngine.cs" company="IrcShark Team">
 // Copyright (C) 2009 IrcShark Team
 // </copyright>
